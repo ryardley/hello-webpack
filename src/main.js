62b945a4fe511c7277ee90267bf5983292f4f49a
Rudi Yardley <contact@rudiyardley.com>
2015-10-18 08:49:12 +0300

    change display a little

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 var greet = require('./greet');
 
-console.log(greet('Webpack'));
+var message = greet('Webpack');
+
+console.log(message);
+
+var elem = document.getElementById('main');
+elem.innerHTML = '<h1>' + message + '</h1><p>A basic webpack starter for testing basic things</p>';
